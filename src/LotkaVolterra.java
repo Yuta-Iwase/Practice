56f371eb3611c2d20a9f6afa94421306b097a694
Owner <Owner@157.80.183.192>
2017-07-04 09:52:56 +0900

    LotkaVolterra 0703

--- src/LotkaVolterra.java
+++ src/LotkaVolterra.java
@@ -5,22 +5,22 @@ public class LotkaVolterra{
 	public static void main(String[] args) throws Exception{
 		// パラメータ定義
 		int T = 10;
-		int u0 = 500;
-		int v0 = 10000;
+		double u0 = 500;
+		double v0 = 10000;
 		double a = 10;
-		double b = 0.02;
+		double b = 10;
 
 		// 以降計算
 		PrintWriter pw_u = new PrintWriter(new File("LotkaVolterra/predator.csv"));
 		PrintWriter pw_v = new PrintWriter(new File("LotkaVolterra/prey.csv"));
-		int u = u0;
-		int v = v0;
+		double u = u0;
+		double v = v0;
 		pw_u.println(0 + "," + u);
 		pw_v.println(0 + "," + v);
-		int next_u, next_v;
+		double next_u, next_v;
 		for(int i=1;i<=T;i++){
-			next_u = (int)(u + u*(v-b));
-			next_v = (int)(v + v*(a-u));
+			next_u = (u + u*(v-b));
+			next_v = (v + v*(a-u));
 			u = next_u;
 			v = next_v;
 			pw_u.println(i + "," + u);
