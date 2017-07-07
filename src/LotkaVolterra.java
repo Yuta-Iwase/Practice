ef3eec7be71a1cf176b21586df314f7b9d8e91b7
yuta <yuta@UbuntuPC-1203>
2017-07-07 13:30:43 +0900

    LotkaVolterra

--- src/LotkaVolterra.java
+++ src/LotkaVolterra.java
@@ -5,27 +5,27 @@ public class LotkaVolterra{
 	public static void main(String[] args) throws Exception{
 		// パラメータ定義
 		int T = 100;
-		double delta_t = 0.1;
-		double u0 = 500;
-		double v0 = 10000;
-		double a = 10;
-		double b = 10;
+		double delta_t = 0.01;
+		double u0 = 50;
+		double v0 = 100;
+		double a = 60;
+		double b = 40;
 
 		// 以降計算
-		PrintWriter pw_u = new PrintWriter(new File("LotkaVolterra/predator.csv"));
-		PrintWriter pw_v = new PrintWriter(new File("LotkaVolterra/prey.csv"));
+		PrintWriter pw_u = new PrintWriter(new File("LotkaVolterra/predator.dat"));
+		PrintWriter pw_v = new PrintWriter(new File("LotkaVolterra/prey.dat"));
 		double u = u0;
 		double v = v0;
-		pw_u.println(0 + "," + u);
-		pw_v.println(0 + "," + v);
+		pw_u.println(0 + "\t" + u);
+		pw_v.println(0 + "\t" + v);
 		double next_u, next_v;
 		for(int i=1;i<=T;i++){
-			next_u = u + delta_t*v;
-			next_v = -delta_t*u + (1-delta_t*delta_t)*v;
+			next_u = u / (1 - delta_t*(v-b));
+			next_v = v*(1+delta_t*a) - delta_t*v*next_u;
 			u = next_u;
 			v = next_v;
-			pw_u.println(i + "," + u);
-			pw_v.println(i + "," + v);
+			pw_u.println(i + "\t" + u);
+			pw_v.println(i + "\t" + v);
 			System.out.println(u);
 			System.out.println(v);
 			System.out.println();
