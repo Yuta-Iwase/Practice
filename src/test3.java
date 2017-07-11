2fec4b9e39b29f61ef53411a0543d6405cced8e2
Owner <Owner@home-PC>
2017-07-12 06:02:15 +0900

    競合回避

--- src/test3.java
+++ src/test3.java
@@ -31,19 +31,3 @@ public class test3 {
 	}
 
 }
-=======
-
-public class test3 {
-
-	public static void main(String[] args) {
-		int T=100;
-		double x=1.0;
-		
-		for(int i=0;i<T;i++){
-			System.out.println(i + "\t" + x);
-			x = Math.sqrt(2*x);
-		}
-	}
-
-}
->>>>>>> refs/remotes/origin/master
