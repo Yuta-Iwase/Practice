1737109c661939ba05dc4e48aca82a1d3fc04672
Owner <Owner@home-PC>
2017-07-12 06:03:24 +0900

    fix

--- src/test3.java
+++ src/test3.java
@@ -1,17 +1,17 @@
-<<<<<<< HEAD
+
 import java.util.ArrayList;
 
 public class test3 {
 
 	public static void main(String[] args) {
 		ArrayList<ArrayList<Object>> matrix = new ArrayList<ArrayList<Object>>();
-		
+
 		char a = 'a';
 		int b = 1;
 		double c = 3.14;
-		
+
 		int N = 10;
-		
+
 		for(int i=0;i<N;i++){
 			ArrayList<Object> currentList = new ArrayList<Object>();
 			currentList.add(a);
@@ -20,7 +20,7 @@ public class test3 {
 			matrix.add(currentList);
 			a++; b++; c*=1.5;
 		}
-		
+
 		for(int i=0;i<matrix.size();i++){
 			for(int j=0;j<matrix.get(i).size();j++){
 				System.out.print(matrix.get(i).get(j) + " ");
