a24934a5c6e4d6f8c3a07a952752d375831d8926
Owner <Owner@OptiPlex7050>
2017-09-17 19:51:15 +0900

    20170917

--- src/test3.java
+++ src/test3.java
@@ -1,33 +1,56 @@
-
-import java.util.ArrayList;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class test3 {
 
-	public static void main(String[] args) {
-		ArrayList<ArrayList<Object>> matrix = new ArrayList<ArrayList<Object>>();
+	public static void main(String[] args) throws Exception{
+		int sampleNum = 10;
 
-		char a = 'a';
-		int b = 1;
-		double c = 3.14;
+		PrintWriter pw = new PrintWriter(new File("sList.csv"));
+		Scanner scan = null;
 
-		int N = 10;
+		String folderName = null;
+		String listName = null;
 
-		for(int i=0;i<N;i++){
-			ArrayList<Object> currentList = new ArrayList<Object>();
-			currentList.add(a);
-			currentList.add(b);
-			currentList.add(c);
-			matrix.add(currentList);
-			a++; b++; c*=1.5;
-		}
+		File list = null;
 
-		for(int i=0;i<matrix.size();i++){
-			for(int j=0;j<matrix.get(i).size();j++){
-				System.out.print(matrix.get(i).get(j) + " ");
+		int[][] sList = new int[240][sampleNum];
+
+		int index;
+		String indexString;
+		int currentInt = 0;
+		int currentIntIndex;
+		for(int i=0;i<sampleNum;i++) {
+			index = i+1;
+
+			if(index<10) indexString = "0" + index;
+			else indexString = "" + index;
+			folderName = "sample" + indexString;
+
+			listName = folderName + "/property.txt";
+			list = new File(listName);
+
+			scan = new Scanner(list);
+			currentIntIndex = 0;
+			while(scan.hasNext()) {
+				currentInt = scan.nextInt();
+				if(currentIntIndex%4==1) {
+					sList[currentIntIndex/4][i] = currentInt;
+				}
+				currentIntIndex++;
 			}
-			System.out.println();
+			scan.close();
 		}
 
-	}
+		for(int i=0;i<240;i++) {
+			for(int j=0;j<sampleNum-1;j++) {
+				pw.print(sList[i][j] + ",");
+			}
+			pw.print(sList[i][sampleNum-1]);
+			pw.println();
+		}
 
+		pw.close();
+	}
 }
